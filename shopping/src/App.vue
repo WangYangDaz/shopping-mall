b82fe99e7f2b026eee00ca22c62d951a37ae2a71
WangYangDaz <18707212401@163.com>
2017-11-04 08:40:31 +0800

    fist

---
+++ shopping/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <ele-header></ele-header>  
+    </div>
+</template>
+
+<script>
+import header from './component/header/header' 
+export default{
+    components:{
+        'ele-header':header
+    }
+}
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+    div
+        width 300px
+</style>
---
+++ shopping/src/component/header/header.vue
@@ -0,0 +1,31 @@
+.<template>
+    <div class="header">
+        <div class="top">
+            <div class="leftCity"></div> 
+            <div class="search"></div>
+            <a href="javascript:;"></a> 
+        </div>
+        <div class="bottom">
+            <ul class="navs">
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+        </div>
+    </div>
+</template>
+
+<script>
+
+export default {
+
+}
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+    
+</style>
